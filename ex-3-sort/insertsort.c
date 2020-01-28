7aeb0ae9adc96810a54e3380a8a3c3e88fd40330
elhamnadimi <nadimi.mo@yahoo.com>
2020-01-28 16:45:28 +0100

    updated

--- ex-3-sort/insertsort.c
+++ ex-3-sort/insertsort.c
@@ -2,23 +2,29 @@
 #include <stdlib.h>
 #include<stdio.h>
 #include<time.h>
-void insertation_sort(int arr[], int n) //n=size array
+
+// It just calls insert on the elements at indices 1,2,3,…,n−1.
+//for the first FOR, PIVOT is second elem and j is first elem
+//because we want to sort all the elements before arr[i]
+
+void insertation_sort(int arr[], int n)  // O(n^2)
 {
 	int i, j, pivot;
-	for (i=1;i<n;i++)
+	for (i=1;i<n;i++) //n times
 	{
-		pivot=arr[i]; //for the first FOR, PIVOT is second elem and j is first elem
-		j=i-1; //because we want to sort all the elements before arr[i]
+		pivot=arr[i]; // O(1)
+		j=i-1; // O(1)
 
 		/* Move elements of arr[0..i-1], that are 
           greater than pivot, to one position ahead 
           of their current position */
-		while(j>=0&&arr[j]>pivot) 
+
+		while(j>=0&&arr[j]>pivot)  // at most j-1 times
 		{
-			arr[j+1]=arr[j];
-			j=j-1;
+			arr[j+1]=arr[j]; // O(1)      //moving array if bigger than next element
+			j=j-1;           // O(1)
 		}
-			arr[j+1]=pivot;
+			arr[j+1]=pivot; // O(1)    // move pivot one elem forward
 
 	}
 }	
@@ -35,13 +41,28 @@ void printArray(int arr[], int n)
 /* Driver program to test insertion sort */
 int main() 
 { 
-    int arr[] = { 12, 11, 13, 5, 6, 10, 20, 40, 32, 65, 87, 54, 12, 1, 0, 65, 98 }; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
+    int arr[] = {1,2,3,4,5,6,7,8,9,12,23,44,55};  // best case
+    int length = sizeof(arr) / sizeof(arr[0]); 
   	clock_t start=clock();
-    insertation_sort(arr, n); 
+    insertation_sort(arr, length); 
   	clock_t end=clock();
-
-    printArray(arr, n); 
+	printArray(arr, length); 
 	printf("time passed[s] %lf \n", (double)(end-start)/CLOCKS_PER_SEC );  
+
     return 0; 
 } 
+
+
+
+//BEST CASE 
+
+//Insertion sort performs two operations: 
+//it scans through the list, comparing each pair of elements, 
+//and it swaps elements if they are out of order. Each operation
+//contributes to the running time of the algorithm. If the input
+// array is already in sorted order, insertion sort compares O(n)O(n)O(n)
+
+/*WORST CASE 
+The worst case for insertion sort will occur when the input list 
+is in decreasing order. To insert the last element, we need at most
+n−1n-1n−1 comparisons and at most n−1n-1n−1 swaps.*/
--- ex-3-sort/quicksort.c
+++ ex-3-sort/quicksort.c
@@ -3,21 +3,31 @@
 #include<stdio.h>
 #include<time.h>
 
+/*
+    Each time the Quick Sort performs the partition()
+     on the input array, the pivot selected is the 
+     smallest/largest value in the array */
 
 void quicksort(int arr[25],int first,int last){
    int i, j, pivot, temp;
 
    if(first<last){
       pivot=first;
-      i=first;
-      j=last;
+      i=first;  // first index of array
+      j=last;  //last index of array
 
-      while(i<j){
+
+      // while array[left] < pivot
+                //left := left + 1
+      //while array[right] > pivot
+                //right := right - 1
+
+      while(i<j){ // while first ≤ last
          while(arr[i]<=arr[pivot]&&i<last)
             i++;
          while(arr[j]>arr[pivot])
             j--;
-         if(i<j){
+         if(i<j){  //swap array[left] with array[right]
             temp=arr[i];
             arr[i]=arr[j];
             arr[j]=temp;
@@ -27,8 +37,8 @@ void quicksort(int arr[25],int first,int last){
       temp=arr[pivot];
       arr[pivot]=arr[j];
       arr[j]=temp;
-      quicksort(arr,first,j-1);
-      quicksort(arr,j+1,last);
+      quicksort(arr,first,j-1); //left side of pivot is not sorted yet
+      quicksort(arr,j+1,last);  // right side of pivot is not sorted yet! 
 
    }
 }
@@ -57,3 +67,12 @@ int main(){
 
    return 0;
 }
+/*The answer depends on strategy for choosing pivot. 
+In early versions of Quick Sort where leftmost (or rightmost)
+ element is chosen as pivot, the worst occurs in following cases.
+
+1) Array is already sorted in same order.
+2) Array is already sorted in reverse order.
+3) All elements are same (special case of case 1 and 2) */
+
+
